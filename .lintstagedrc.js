b76531427381f8a86b7b863787d01350a2d8a5e7
Jeremiah Brem <jeremiah.brem@gmail.com>
2024-01-22 09:16:11 -0600

    test conditional build-docs in pre-commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,17 @@
 module.exports = {
   "*.(tsx|ts)": (files) => {
-    const defaultCmds = [
-      "prettier --check --write",
-        "eslint"
-    ];
-
-    console.log(files)
-
     const prettify = files.map(x => `prettier --check --write ${x}`);
     const lint = files.map(x => `eslint ${x}`);
-    return [...prettify, ...lint];
+
+    let commands = [...prettify, ...lint];
+
+    if (files.find(x => x.match(/index.ts/)) || files.find(x => x.match(/types.ts/))) {
+      commands = [
+        "npm run build-docs",
+        ...commands
+      ]
+    }
+
+    return commands;
   },
 }
