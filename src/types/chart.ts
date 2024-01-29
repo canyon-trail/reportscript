8ef8976b423fb1c8a7c871e0b674d9c143c9d997
Floyd May <floyd.may@gmail.com>
2024-01-29 15:21:49 -0600

    Fix doc comment

--- src/types/chart.ts
+++ src/types/chart.ts
@@ -28,9 +28,8 @@ export type Chart<
    * page break to be inserted before the chart.
    */
   minHeight: number;
-  /** maximum height of the chart in points. Prevents fitting the chart
-   * into too small of a vertical area and will cause a
-   * page break to be inserted before the chart.
+  /** maximum height of the chart in points. Helps to prevent the chart from taking up too
+   * much vertical space
    */
   maxHeight?: number;
   /**
